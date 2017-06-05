e23d9ea6eee7d880425fda4a5adc00429d94c0b8
disne_y <bold.disney@gmail.com>
2017-06-05 14:01:31 +0300

    Update main file

--- main.rb
+++ main.rb
@@ -1,18 +1,21 @@
 require_relative 'game_controller.rb'
 require_relative 'bank.rb'
 require_relative 'card_deck.rb'
-require_relative 'dealer.rb'
+
 require_relative 'player.rb'
+require_relative 'dealer.rb'
+require_relative 'user.rb'
+
 
 puts 'Добро пожаловать в ЛуноПарк'
 
 controller = GameController.new
-controller.create_player
-controlelr.create_dealer
+controller.start_game
 
-loop do
-  # controller.display_actions
-  # user_choice = gets.strip
-  # break if user_choice == '0'
-  # controller.render_action(user_choice)
-end
+
+# loop do
+#   # controller.display_actions
+#   # user_choice = gets.strip
+#   # break if user_choice == '0'
+#   # controller.render_action(user_choice)
+# end
