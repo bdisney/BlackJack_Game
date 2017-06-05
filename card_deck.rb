03c8649655aa74c78c2e43be1a3b128e3e60b79f
disne_y <bold.disney@gmail.com>
2017-06-05 14:01:09 +0300

    Players can take cards

--- card_deck.rb
+++ card_deck.rb
@@ -1,24 +1,26 @@
 class CardDeck
-  SUITS = ["\u{2664}", "\u{2661}", "\u{2667}", "\u{2662}"].freeze
+  LEARS = ["\u{2664}", "\u{2661}", "\u{2667}", "\u{2662}"].freeze
   CARD_VALUES = {
     '2' => 2, '3'  => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
     '9' => 9, '10' => 10, 'K' => 10, 'J' => 10, 'Q' => 10, 'A' => 1
   }.freeze
 
-  attr_reader :deck
+  attr_accessor :deck
 
   def initialize
+    @deck = []
     create
   end
 
   def create
-    @deck = []
-
-    SUITS.each do |suit|
-      CARD_VALUES.each do |card, value|
-        @deck << [card + suit, value]
-      end
+    LEARS.each do |lear|
+      CARD_VALUES.each { |card, value| deck << [card + lear, value] }
     end
-    @deck.shuffle!
+    deck.shuffle!
+  end
+
+  def give_out_card
+    puts 'Выдаем карту'
+    @deck.pop
   end
 end
--- game_controller.rb
+++ game_controller.rb
@@ -1,21 +1,49 @@
 class GameController
+  attr_reader :deck
+
+  def initialize
+    @bank = Bank.new(:game, 0)
+  end
+
+  def start_game
+    create_player
+    create_dealer
+    create_deck
+    game
+  end
 
   def create_player
     print 'Введите Ваше имя: '
     name = gets.strip.downcase.capitalize
-    player = Player.new(name)
+
+    @user = User.new(name)
+
+    @user ? (puts "Hello, #{@user.name}! Let the fun begins!") : (raise "You should enter your name first")
   end
 
   def create_dealer
-    dealer = Dealer.new
+    @dealer = Dealer.new
   end
 
   def create_deck
-    deck = CardDeck.new
+    @deck = CardDeck.new
+    puts @deck.inspect
   end
-  # Let's the Game begin
-  # create banks
-  # create card deck
 
+  def game
+    first_move
+    @user.display_cards
+  end
 
+  def first_move
+    bet
+    2.times do
+      [@user, @dealer].each { |player| player.take_card(@deck.give_out_card) }
+    end
+  end
+
+  def bet(value = 10)
+    [@user.account, @dealer.account].each { |player| player.money_transfer(@bank, value) }
+    puts "Счет игрока: #{@user.account.sum}, счет дилера: #{@dealer.account.sum}. В банке: #{@bank.sum}"
+  end
 end
