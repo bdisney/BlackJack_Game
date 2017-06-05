6c1a4c4ff86e43a411783132c8d6836b149cdd16
disne_y <bold.disney@gmail.com>
2017-06-05 13:59:05 +0300

    Add money_transfer method

--- bank.rb
+++ bank.rb
@@ -1,5 +1,12 @@
 class Bank
-  def initialize
+  attr_accessor :sum
+
+  def initialize(holder, sum)
     @sum = sum
+  end
+
+  def money_transfer(receiver, value)
+    self.sum -= value
+    receiver.sum += value
   end
 end
